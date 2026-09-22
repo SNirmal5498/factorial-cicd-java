b2acddb9c3a0523118dc99eaaf61413c9051e765
Nirmal-S <nirmalsundaram5498@gmail.com>
2026-09-22 21:18:55 +0530

    Corrected  CI test

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -10,7 +10,7 @@ class FactorialTest {
 
     @Test
     void testFactorialOfFive() {
-        assertEquals(125, Factorial.calculate(5));
+        assertEquals(120, Factorial.calculate(5));
     }
 
     @Test
